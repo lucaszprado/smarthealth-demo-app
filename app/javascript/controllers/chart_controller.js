e28de2964adc9e0873259735b8e1205e49c7c688
lucasprado <lucaspradobr@gmail.com>
2024-10-14 11:06:35 -0300

    chart refining

--- app/javascript/controllers/chart_controller.js
+++ app/javascript/controllers/chart_controller.js
@@ -11,7 +11,7 @@ export default class extends Controller {
     const ctx = this.canvasTarget.getContext("2d");
 
     // Example chart data and options, you can customize as needed
-    const labels = [1,2,3,4,5,6,7];
+    const labels = ['2024-01-01', '2024-01-02', '2024-01-03', '2024-01-04', '2024-01-05', '2024-01-06', '2024-01-07'];
     const upperBandY = 85;
     const lowerBandY = 45;
 
@@ -20,7 +20,7 @@ export default class extends Controller {
       datasets: [
       {
         label: 'Biomarker measures',
-        data: [65, 59, 80, 90, 56, 55, 40],
+        data: [65, 59, 80, 90, 56, 55, 40], // Biomarker measures
         fill: false,
         borderColor: 'rgba(255, 136, 91, 0.5)',
         tension: 0.1,
@@ -39,7 +39,7 @@ export default class extends Controller {
       },
       {
         label: 'Lower band',
-        data: [45, 45, 45, 45, 45, 45, 45],
+        data: [lowerBandY, lowerBandY, lowerBandY, lowerBandY, lowerBandY, lowerBandY, lowerBandY],
         fill: false,
         borderColor: 'rgba(129, 176, 239, 0.3)',
         tension: 0.1,
@@ -58,9 +58,9 @@ export default class extends Controller {
 
             // Abrupt fill up to y=45
             gradient.addColorStop(0, 'rgba(204, 229, 255, 0.2)'); // Solid fill from the top (y=85) to y=45
-            gradient.addColorStop(0.55, 'rgba(204, 229, 255, 0.2)'); // Still solid at y=45
-            gradient.addColorStop(0.55, 'rgba(255, 99, 132, 0)');   // Abruptly changes to transparent at y=45
-            gradient.addColorStop(1, 'rgba(255, 99, 132, 0)');      // Fully transparent below y=45
+            gradient.addColorStop(0.73, 'rgba(204, 229, 255, 0.2)'); // Still solid at y=45
+            gradient.addColorStop(0.73, 'rgba(255, 99, 132, 0)');   // Abruptly changes to transparent at y=45
+            gradient.addColorStop(0.73, 'rgba(255, 99, 132, 0)');      // Fully transparent below y=45
 
             return gradient;
           },
@@ -72,27 +72,45 @@ export default class extends Controller {
     };
 
     const options = {
-          scales: {
-            y: {
-              min: 0,
-              max: 100,
-              ticks: {
-                stepSize: 10
-              }
-            }
+      scales: {
+        x: {
+         // type: 'timeseries',
+          grid: {
+            display: false,
           },
-          plugins: {
-            tooltip: {
-              callbacks: {
-                // Custom label to display only the y-axis value in the tooltip
-                label: function(tooltipItem) {
-                // Return only the y-axis value (which is the "raw" value of the point)
-                return `${tooltipItem.raw}`;
-                }
-              }
+          //offset: true,
+          ticks: {
+            padding: 1,
+          }
+        },
+        y: {
+          grid:{
+            display: false
+          },
+          min: 30,
+          max: 100,
+          ticks: {
+            stepSize: 10,
+            padding: 30,
+            display: true
+          },
+          border: {
+            display: false
+          }
+        }
+      },
+      plugins: {
+        tooltip: {
+          callbacks: {
+            // Custom label to display only the y-axis value in the tooltip
+            label: function(tooltipItem) {
+            // Return only the y-axis value (which is the "raw" value of the point)
+            return `${tooltipItem.raw}`;
             }
           }
-        };
+        }
+      }
+    };
 
     // Initialize the chart
     new Chart(ctx, {
