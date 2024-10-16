1da0755790ce7dff914566402cde5d88b90b36f5
Joaquim Oliveira <joaquim.oliveira.nt@gmail.com>
2024-10-16 11:06:06 -0300

    added synonyms on admin

--- app/models/synonym.rb
+++ app/models/synonym.rb
@@ -1,6 +1,10 @@
 class Synonym < ApplicationRecord
   belongs_to :biomarker
 
+  def self.ransackable_associations(auth_object = nil)
+    ["biomarker"]
+  end
+
   def self.ransackable_attributes(auth_object = nil)
     ["biomarker_id", "created_at", "id", "id_value", "language", "name", "updated_at"]
   end
