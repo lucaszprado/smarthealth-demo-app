e23982dae50319ebb7a7f2e42a38b9fec67b8cae
lucasprado <lucaspradobr@gmail.com>
2024-10-30 14:34:02 -0300

    pop-up front-end fixes

--- app/javascript/controllers/chart_controller.js
+++ app/javascript/controllers/chart_controller.js
@@ -185,9 +185,6 @@ export default class extends Controller {
       },
     };
 
-    // const chartContainer = document.querySelector('.chart-container'); // Assuming you have a container
-    // chartContainer.style.minHeight = '20rem';
-
     // debugger
     // Initialize the chart
     new Chart(ctx, {
