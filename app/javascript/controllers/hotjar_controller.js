fb9873ec3107865e3137abc067fea8b7a47eb8f2
lucasprado <lucaspradobr@gmail.com>
2024-10-31 12:02:45 -0300

    Pop-up Feedback: Hotjar - Turbo Links Workaround

--- app/javascript/controllers/hotjar_controller.js
+++ app/javascript/controllers/hotjar_controller.js
@@ -3,22 +3,19 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="hotjar"
 export default class extends Controller {
   connect() {
-
     console.log("Hello from hotjar controller")
     this.loadHotjar();
-
   }
 
   loadHotjar() {
-
     // Remove existing script if it exists to reload it
     const existingScript = document.querySelector('script[src^="https://static.hotjar.com/c/hotjar-"]');
     if (existingScript) {
-      console.log("Re-triggering Hotjar on turbo navigation");
+      console.log("Removing existing script");
       existingScript.remove();
     }
 
-    console.log("Initializing Hotjar for the first time");
+    console.log("Re-loading Hotjar script");
     (function(h, o, t, j, a, r) {
       h.hj = h.hj || function() { (h.hj.q = h.hj.q || []).push(arguments); };
       h._hjSettings = { hjid:5191202, hjsv: 6 };
@@ -29,11 +26,3 @@ export default class extends Controller {
     })(window, document, 'https://static.hotjar.com/c/hotjar-', '.js?sv=');
   }
 }
-
-// // Ensure Hotjar is re-triggered on Turbo navigation
-// document.addEventListener('turbo:load', () => {
-//   const hotjarElement = document.querySelector('[data-controller="hotjar"]');
-//   if (hotjarElement && hotjarElement.controller) {
-//     hotjarElement.controller.loadHotjar();
-//   }
-// });
