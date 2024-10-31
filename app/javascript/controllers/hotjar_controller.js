13d164e1e8cd1329a279a68a60033a3f4b6f637b
lucasprado <lucaspradobr@gmail.com>
2024-10-31 12:19:31 -0300

    Pop-up Feedback: Hotjar - Turbo Link Workaround

--- app/javascript/controllers/hotjar_controller.js
+++ app/javascript/controllers/hotjar_controller.js
@@ -15,13 +15,16 @@ export default class extends Controller {
       existingScript.remove();
     }
 
-    console.log("Re-loading Hotjar script");
+    const timestamp = new Date().getTime(); // Add const here
+
+    console.log("Re-loading Hotjar script with cache bypass");
     (function(h, o, t, j, a, r) {
       h.hj = h.hj || function() { (h.hj.q = h.hj.q || []).push(arguments); };
       h._hjSettings = { hjid:5191202, hjsv: 6 };
       a = o.getElementsByTagName('head')[0];
       r = o.createElement('script'); r.async = 1;
-      r.src = t + h._hjSettings.hjid + j + h._hjSettings.hjsv;
+      // r.src = t + h._hjSettings.hjid + j + h._hjSettings.hjsv;
+      r.src = `${t + h._hjSettings.hjid + j + h._hjSettings.hjsv}&cacheBuster=${timestamp}`;
       a.appendChild(r);
     })(window, document, 'https://static.hotjar.com/c/hotjar-', '.js?sv=');
   }
