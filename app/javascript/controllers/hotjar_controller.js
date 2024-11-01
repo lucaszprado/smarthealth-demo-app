14b7d18f3df7b69e9f9390f2ddfe30e6b7b14d99
lucasprado <lucaspradobr@gmail.com>
2024-10-31 23:36:51 -0300

    hotjar fix

--- app/javascript/controllers/hotjar_controller.js
+++ app/javascript/controllers/hotjar_controller.js
@@ -15,30 +15,36 @@ export default class extends Controller {
 
   handleTurboLoad() {
     console.log("turbo:load event detected");
-    this.loadHotjar();
+    this.reloadHotjar();
   }
 
   loadHotjar() {
     console.log("loadHotjar() called");
-    // Check if the Hotjar script is already loaded, and run it if necessary
-    if (typeof window.hj === "function") {
-      console.log("Reinvoking Hotjar tracking function");
-      window.hj('trigger', 'page_view'); // This will re-trigger Hotjar tracking for a new page view
-    } else {
-      console.log("Initializing Hotjar script");
 
-    //const timestamp = new Date().getTime(); // Add const here
-      (function(h, o, t, j, a, r) {
-        h.hj = h.hj || function() { (h.hj.q = h.hj.q || []).push(arguments); };
-        h._hjSettings = { hjid:5191202, hjsv: 6 };
-        a = o.getElementsByTagName('head')[0];
-        r = o.createElement('script'); r.async = 1;
-        r.src = t + h._hjSettings.hjid + j + h._hjSettings.hjsv;
-        // r.src = `${t + h._hjSettings.hjid + j + h._hjSettings.hjsv}&cacheBuster=${timestamp}`;
-        a.appendChild(r);
-      })(window, document, 'https://static.hotjar.com/c/hotjar-', '.js?sv=');
+    (function(h, o, t, j, a, r) {
+      h.hj = h.hj || function() { (h.hj.q = h.hj.q || []).push(arguments); };
+      h._hjSettings = { hjid:5191202, hjsv: 6 };
+      a = o.getElementsByTagName('head')[0];
+      r = o.createElement('script'); r.async = 1;
+      r.src = t + h._hjSettings.hjid + j + h._hjSettings.hjsv;
+      a.appendChild(r);
+    })(window, document, 'https://static.hotjar.com/c/hotjar-', '.js?sv=');
 
-      console.log("Hotjar script added to the DOM");
-    }
+    console.log("Hotjar script added to the DOM");
   }
+
+  reloadHotjar() {
+    console.log("Reloading Hotjar script");
+
+    // Remove existing script tag if present
+    const existingScript = document.querySelector('script[src^="https://static.hotjar.com"]');
+    if (existingScript) {
+      existingScript.remove();
+      console.log("Existing Hotjar script removed");
+    }
+
+    // Re-add the Hotjar script
+    this.loadHotjar();
+  }
+
 }
