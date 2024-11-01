0a178b7fa256821e7ee73b02bb7397196d7f92df
lucasprado <lucaspradobr@gmail.com>
2024-10-31 23:02:46 -0300

    hotjar fixes

--- app/javascript/controllers/hotjar_controller.js
+++ app/javascript/controllers/hotjar_controller.js
@@ -7,35 +7,36 @@ export default class extends Controller {
     this.loadHotjar();
 
      // Ensure the script runs when the page is visited through a link
-     document.addEventListener("turbo:load", this.loadHotjar);
-
+     document.addEventListener("turbo:load", () => {
+      requestAnimationFrame(() => {
+        this.loadHotjar();
+      });
+    });
   }
 
   disconnect() {
     // Clean up the event listener when the controller is disconnected
     document.removeEventListener("turbo:load", this.loadHotjar);
   }
-  
+
 
   loadHotjar() {
-    // Remove existing script if it exists to reload it
-    const existingScript = document.querySelector('script[src^="https://static.hotjar.com/c/hotjar-"]');
-    if (existingScript) {
-      console.log("Removing existing script");
-      existingScript.remove();
+    // Check if the Hotjar script is already loaded, and run it if necessary
+    if (typeof window.hj === "undefined" || !window.hj) {
+      console.log("Hotjar script initializing");
+
+    //const timestamp = new Date().getTime(); // Add const here
+      (function(h, o, t, j, a, r) {
+        h.hj = h.hj || function() { (h.hj.q = h.hj.q || []).push(arguments); };
+        h._hjSettings = { hjid:5191202, hjsv: 6 };
+        a = o.getElementsByTagName('head')[0];
+        r = o.createElement('script'); r.async = 1;
+        r.src = t + h._hjSettings.hjid + j + h._hjSettings.hjsv;
+        // r.src = `${t + h._hjSettings.hjid + j + h._hjSettings.hjsv}&cacheBuster=${timestamp}`;
+        a.appendChild(r);
+      })(window, document, 'https://static.hotjar.com/c/hotjar-', '.js?sv=');
+
+      console.log("Hotjar script loaded");
     }
-
-    const timestamp = new Date().getTime(); // Add const here
-
-    console.log("Re-loading Hotjar script with cache bypass");
-    (function(h, o, t, j, a, r) {
-      h.hj = h.hj || function() { (h.hj.q = h.hj.q || []).push(arguments); };
-      h._hjSettings = { hjid:5191202, hjsv: 6 };
-      a = o.getElementsByTagName('head')[0];
-      r = o.createElement('script'); r.async = 1;
-      // r.src = t + h._hjSettings.hjid + j + h._hjSettings.hjsv;
-      r.src = `${t + h._hjSettings.hjid + j + h._hjSettings.hjsv}&cacheBuster=${timestamp}`;
-      a.appendChild(r);
-    })(window, document, 'https://static.hotjar.com/c/hotjar-', '.js?sv=');
   }
 }
