9d9b51b34136169e6aba56856ba8ec17e609f70f
lucasprado <lucaspradobr@gmail.com>
2025-02-25 16:32:09 -0300

    search function | build search

--- app/controllers/imaging_reports_controller.rb
+++ app/controllers/imaging_reports_controller.rb
@@ -1,14 +1,33 @@
 class ImagingReportsController < ApplicationController
   def index
     @human = Human.find(params[:human_id])
-
     # 1. Build ActiveRecord collection of imaging_reports -> Simillar an array
-    imaging_reports_collection = ImagingReport
-      .joins(source: :human)
-      .joins(:imaging_method)
-      .where(sources: {human_id: @human.id})
-      .includes(:labels)
-      .order(date: :desc)
+    
+    if params[:query].present?
+      #Pre fixing matching with OR conditions
+      search_query = params[:query].split.map {|term| "#{term}:*"}.join(" | ")
+
+      imaging_reports_collection = ImagingReport
+        .joins(source: :human)
+        .joins(:imaging_method)
+        .left_joins(:labels)
+        .where(sources: {human_id: @human.id})
+        .where(
+          "to_tsvector('portuguese', imaging_reports.content) @@ to_tsquery('portuguese', :query) OR to_tsvector('portuguese', COALESCE(labels.name, '')) @@ to_tsquery('portuguese', :query)",
+          query: search_query
+        )
+        .distinct
+        .includes(:labels)
+        .order(date: :desc)
+
+    elsif
+      imaging_reports_collection = ImagingReport
+        .joins(source: :human)
+        .joins(:imaging_method)
+        .where(sources: {human_id: @human.id})
+        .includes(:labels)
+        .order(date: :desc)
+    end
 
     # Since imaging_reports ActiveRecord collection of ImagingReports, Rails (Ruby specifically) looks for a method inside ImagingReport Class (model)
     # @imaging_reports is an array of hashs
