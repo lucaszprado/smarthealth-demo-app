299d96c07e34acd9f405ba0de41af2078e69be10
lucasprado <lucaspradobr@gmail.com>
2025-03-11 11:31:43 -0300

    create relation between biomarkers and human

--- app/models/human.rb
+++ app/models/human.rb
@@ -2,6 +2,7 @@ class Human < ApplicationRecord
   has_many :sources, dependent: :destroy
   has_many :measures, through: :sources
   has_many :imaging_reports, through: :sources
+  has_many :biomarkers, -> { distinct }, through: :measures
 
   def self.ransackable_associations(auth_object = nil)
     ["sources", "measures", "imaging_reports"]
