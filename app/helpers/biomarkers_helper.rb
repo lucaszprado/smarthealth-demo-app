4e4c3a0d8e290d87c2633076b1d944c0149d83bc
lucasprado <lucaspradobr@gmail.com>
2025-03-12 11:15:58 -0300

    Refactor view using biomarker helper

--- app/helpers/biomarkers_helper.rb
+++ app/helpers/biomarkers_helper.rb
@@ -1,2 +1,13 @@
 module BiomarkersHelper
+  def result_card_attributes(human, biomarker)
+    {
+      link: human_biomarker_measures_path(human.id, biomarker[:biomarker_id]),
+      icon_path: "fa-solid fa-vial",
+      title: biomarker[:synonym_name],
+      key_info: biomarker[:unit_value_type] == 1 ? "#{biomarker[:original_value]} #{biomarker[:unit_name]}" : biomarker[:measure_text],
+      status: biomarker[:unit_value_type] == 1 ? biomarker[:measure_status] : nil,
+      date: biomarker[:date],
+      labels: nil
+    }
+  end
 end
