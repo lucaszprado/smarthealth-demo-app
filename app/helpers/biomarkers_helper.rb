adddbe5408849a74e4ac55e817a07d9ca004cf46
lucasprado <lucaspradobr@gmail.com>
2025-04-14 20:39:06 -0300

    no data front end integrated and refactored to comply with front-end rules

--- app/helpers/biomarkers_helper.rb
+++ app/helpers/biomarkers_helper.rb
@@ -3,7 +3,7 @@ module BiomarkersHelper
   def result_card_attributes(human, biomarker)
     {
       link: human_biomarker_measures_path(human.id, biomarker[:biomarker_id]),
-      icon_path: "fa-solid fa-vial",
+      icon_path: biomarker[:source_type_name] == "Blood" ? "fa-solid fa-vial" : "fa-solid fa-weight-scale",
       title: biomarker[:display_name],
       key_info: biomarker[:unit_value_type] == 1 ? "#{format_value_2_decimals(biomarker[:original_value])} #{biomarker[:unit_name]}" : biomarker[:measure_text],
       status: biomarker[:unit_value_type] == 1 ? biomarker[:measure_status] : nil,
@@ -12,4 +12,13 @@ module BiomarkersHelper
       data_type: biomarker[:source_type_name]
     }
   end
+
+  def render_biomarkers_partial(biomarkers)
+    if biomarkers.empty?
+      'shared/no_data'
+    else
+      'biomarkers/data'
+    end
+  end
+
 end
--- app/helpers/imaging_reports_helper.rb
+++ app/helpers/imaging_reports_helper.rb
@@ -1,2 +1,9 @@
 module ImagingReportsHelper
+  def render_imaging_reports_partial(imaging_reports)
+    if imaging_reports.empty?
+      'shared/no_data'
+    else
+      'imaging_reports/data'
+    end
+  end
 end
